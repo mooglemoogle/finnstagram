223b2a9afc99c06af90a6e0c501e6250514b2e00
Chris Rock <chris.rock@logianalytics.com>
2020-06-30 10:14:05 -0400

    Add padding element to bottom of feed

--- App.tsx
+++ App.tsx
@@ -1,25 +1,38 @@
-import React, { FC } from 'react';
-import { View, StatusBar, ImageURISource, ScrollView, Dimensions } from 'react-native';
+import React, { FC } from "react";
+import {
+  View,
+  StatusBar,
+  ImageURISource,
+  ScrollView,
+  Dimensions,
+} from "react-native";
 
-import { Post } from './components/post/post';
-import { Header } from './components/header/header';
+import { Post } from "./components/post/post";
+import { Header } from "./components/header/header";
 
 const App: FC = () => {
-    const img: ImageURISource = {uri: 'http://engagement.abysmalsoft.org.s3.amazonaws.com/Engagement%20Photo%203.jpg'}
+  const img: ImageURISource = {
+    uri:
+      "http://engagement.abysmalsoft.org.s3.amazonaws.com/Engagement%20Photo%203.jpg",
+  };
 
-    return (
-        <View >
-            <StatusBar barStyle={'dark-content'} />
-            <Header />
-            <ScrollView style={{paddingTop: 90, paddingBottom: 90}} showsVerticalScrollIndicator={false}>
-                <Post image={img} />
-                <Post image={img} />
-                <Post image={img} />
-                <Post image={img} />
-                <Post image={img} />
-            </ScrollView>
-        </View>
-    );
+  return (
+    <View>
+      <StatusBar barStyle={"dark-content"} />
+      <Header />
+      <ScrollView
+        style={{ paddingTop: 90 }}
+        showsVerticalScrollIndicator={false}
+      >
+        <Post image={img} />
+        <Post image={img} />
+        <Post image={img} />
+        <Post image={img} />
+        <Post image={img} />
+        <View style={{ height: 120 }} />
+      </ScrollView>
+    </View>
+  );
 };
 
 export default App;
